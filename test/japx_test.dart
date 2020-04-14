841fc153abfda816c98b733ac22db7e554911a32
Dundo7 <maroje.marcelic@gmail.com>
2020-04-14 22:05:04 +0200

    Added readme, tests and soruce code

--- test/japx_test.dart
+++ test/japx_test.dart
@@ -1,13 +1,26 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:japx/japx.dart';
+import '../lib/src/parser.dart';
+import 'test_data.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Basic decoding', () async => compare(japxDecode(await decodingSample1()), await resultDecoding1()));
+  test('Relationship decoding', () async => compare(japxDecode(await decodingSample2()), await resultDecoding2()));
+  test('Additional info decoding', () async => compare(japxDecode(await decodingSample3()), await resultDecoding3()));
+  test('Recursive decoding', () async => compare(japxDecode(await decodingSample4()), await resultDecoding4()));
+  test('Relationship no include decoding',
+          () async => compare(japxDecode(await decodingSample5()), await resultDecoding5()));
+  test('Advanced decoding', () async => compare(japxDecode(await decodingSample6()), await resultDecoding6()));
+  test(
+      'Empty relationship decoding', () async => compare(japxDecode(await decodingSample7()), await resultDecoding7()));
+
+  test('Basic encoding', () async => compare(japxEncode(await encodingSample1()), await resultEncoding1()));
+  test('Extra params encoding', () async => compare(japxEncode(await encodingSample2()), await resultEncoding2()));
+  test('Meta encoding', () async => compare(japxEncode(await encodingSample3()), await resultEncoding3()));
+  test('Recursive encoding', () async => compare(japxEncode(await encodingSample4()), await resultEncoding4()));
+  test('Simple encoding', () async => compare(japxEncode(await encodingSample5()), await resultEncoding5()));
 }
+
+void compare(Map<String, dynamic> sample, Map<String, dynamic> result) => expect(sample, result);
