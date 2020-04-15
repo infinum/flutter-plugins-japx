fd24f62d20bd443d9286e437348b887b5c2f4e3e
Dundo7 <maroje.marcelic@gmail.com>
2020-04-15 11:51:13 +0200

    Added example

---
+++ example/android/app/src/main/kotlin/com/infinum/japx_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.infinum.japx_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/user_model.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+/// Using [JsonSerializable] to parse simple data object
+/// After changing run:
+///
+/// flutter pub run build_runner build
+///
+/// to rebuild generated files needed for parsing to and from json
+@JsonSerializable(nullable: false)
+class User {
+  User(this.email, this.name);
+  factory User.fromMap(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toMap() => _$UserToJson(this);
+
+  final String email;
+  final String name;
+
+  @override
+  String toString() {
+    return 'User email: $email and name: $name';
+  }
+}
