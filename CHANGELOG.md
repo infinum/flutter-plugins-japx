676adc22a06f64adce3f537e6f32ca9acf94b6ee
Khaleel Shaheen <khaleel_sh@hotmail.com>
2022-11-07 00:35:37 +0200

    Add linting rules and format code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,39 +1,43 @@
-## 1.0.0
+## 2.0.5
 
-* Initial release with created Japx package.
-
-## 1.0.1
-
-* Updated package documentation.
-* Resolved maintenance issues and suggestions.
-
-## 1.0.2
-
-* Fixed an issue when encoding a list.
-* Added a test for encoding a list.
-
-## 1.0.3
-
-* Updated image.
-* Fixed formatting.
-* Removed installation from README.
-
-## 2.0.0
-
-* Added null safety
-
-## 2.0.1
-
-* Internal bugfixes
-
-## 2.0.2
-
-* Internal bugfixes
-
-## 2.0.3
-
-* Internal bugfixes - Tnx to MRSEREY for contribution :)
+- Add linting rules from `lints` package
 
 ## 2.0.4
 
-* Remove Flutter dependency
+- Remove Flutter dependency
+
+## 2.0.3
+
+- Internal bugfixes - Tnx to MRSEREY for contribution :)
+
+## 2.0.2
+
+- Internal bugfixes
+
+## 2.0.1
+
+- Internal bugfixes
+
+## 2.0.0
+
+- Added null safety
+
+## 1.0.3
+
+- Updated image.
+- Fixed formatting.
+- Removed installation from README.
+
+## 1.0.2
+
+- Fixed an issue when encoding a list.
+- Added a test for encoding a list.
+
+## 1.0.1
+
+- Updated package documentation.
+- Resolved maintenance issues and suggestions.
+
+## 1.0.0
+
+- Initial release with created Japx package.
--- test/test_data.dart
+++ test/test_data.dart
@@ -1,31 +1,44 @@
 import 'dart:convert';
 import 'dart:io';
 
-Future<Map<String, dynamic>> decodingSample1() async => parseJsonFromPath('assets/decoding/decoding-1.json');
+Future<Map<String, dynamic>> decodingSample1() async =>
+    parseJsonFromPath('assets/decoding/decoding-1.json');
 
-Future<Map<String, dynamic>> decodingSample2() async => parseJsonFromPath('assets/decoding/decoding-2.json');
+Future<Map<String, dynamic>> decodingSample2() async =>
+    parseJsonFromPath('assets/decoding/decoding-2.json');
 
-Future<Map<String, dynamic>> decodingSample3() async => parseJsonFromPath('assets/decoding/decoding-3.json');
+Future<Map<String, dynamic>> decodingSample3() async =>
+    parseJsonFromPath('assets/decoding/decoding-3.json');
 
-Future<Map<String, dynamic>> decodingSample4() async => parseJsonFromPath('assets/decoding/decoding-4.json');
+Future<Map<String, dynamic>> decodingSample4() async =>
+    parseJsonFromPath('assets/decoding/decoding-4.json');
 
-Future<Map<String, dynamic>> decodingSample5() async => parseJsonFromPath('assets/decoding/decoding-5.json');
+Future<Map<String, dynamic>> decodingSample5() async =>
+    parseJsonFromPath('assets/decoding/decoding-5.json');
 
-Future<Map<String, dynamic>> decodingSample6() async => parseJsonFromPath('assets/decoding/decoding-6.json');
+Future<Map<String, dynamic>> decodingSample6() async =>
+    parseJsonFromPath('assets/decoding/decoding-6.json');
 
-Future<Map<String, dynamic>> decodingSample7() async => parseJsonFromPath('assets/decoding/decoding-7.json');
+Future<Map<String, dynamic>> decodingSample7() async =>
+    parseJsonFromPath('assets/decoding/decoding-7.json');
 
-Future<Map<String, dynamic>> encodingSample1() async => parseJsonFromPath('assets/encoding/encoding-1.json');
+Future<Map<String, dynamic>> encodingSample1() async =>
+    parseJsonFromPath('assets/encoding/encoding-1.json');
 
-Future<Map<String, dynamic>> encodingSample2() async => parseJsonFromPath('assets/encoding/encoding-2.json');
+Future<Map<String, dynamic>> encodingSample2() async =>
+    parseJsonFromPath('assets/encoding/encoding-2.json');
 
-Future<Map<String, dynamic>> encodingSample3() async => parseJsonFromPath('assets/encoding/encoding-3.json');
+Future<Map<String, dynamic>> encodingSample3() async =>
+    parseJsonFromPath('assets/encoding/encoding-3.json');
 
-Future<Map<String, dynamic>> encodingSample4() async => parseJsonFromPath('assets/encoding/encoding-4.json');
+Future<Map<String, dynamic>> encodingSample4() async =>
+    parseJsonFromPath('assets/encoding/encoding-4.json');
 
-Future<Map<String, dynamic>> encodingSample5() async => parseJsonFromPath('assets/encoding/encoding-5.json');
+Future<Map<String, dynamic>> encodingSample5() async =>
+    parseJsonFromPath('assets/encoding/encoding-5.json');
 
-Future<Map<String, dynamic>> encodingSample6() async => parseJsonFromPath('assets/encoding/encoding-6.json');
+Future<Map<String, dynamic>> encodingSample6() async =>
+    parseJsonFromPath('assets/encoding/encoding-6.json');
 
 Future<Map<String, dynamic>> resultDecoding1() async =>
     parseJsonFromPath('assets/result/decoding/result-decoding-1.json');
@@ -67,7 +80,8 @@ Future<Map<String, dynamic>> resultEncoding6() async =>
     parseJsonFromPath('assets/result/encoding/result-encoding-6.json');
 
 Future<Map<String, dynamic>> parseJsonFromPath(String assetsPath) async {
-  Map<String, dynamic>? json = jsonDecode(await File(assetsPath).readAsString());
+  Map<String, dynamic>? json =
+      jsonDecode(await File(assetsPath).readAsString());
   if (json == null) {
     throw 'Unable to read file $assetsPath';
   }
